a2818b229fd2f1b4e6a0c65c4c033cebc9ea65b9
kherincalderon <kherincalderon@gmail.com>
2025-01-17 02:58:10 -0600

    [init] project

---
+++ components/MobileNav.jsx
@@ -0,0 +1,81 @@
+'use client';
+import {
+  Sheet,
+  SheetContent,
+  SheetTrigger,
+  SheetTitle,
+} from '@/components/ui/sheet';
+import * as VisuallyHidden from '@radix-ui/react-visually-hidden';
+
+import { usePathname } from 'next/navigation';
+import Link from 'next/link';
+import { CiMenuFries } from 'react-icons/ci';
+
+const links = [
+  {
+    name: 'home',
+    path: '/',
+  },
+  {
+    name: 'services',
+    path: '/services',
+  },
+  {
+    name: 'resume',
+    path: '/resume',
+  },
+  {
+    name: 'work',
+    path: '/work',
+  },
+  {
+    name: 'contact',
+    path: '/contact',
+  },
+];
+
+const MobileNav = () => {
+  const pathname = usePathname();
+  return (
+    <Sheet>
+      <VisuallyHidden.Root>
+        <SheetTitle>Menu</SheetTitle>
+      </VisuallyHidden.Root>
+
+      <SheetTrigger className="flex justify-center items-center">
+        <CiMenuFries className="text-[32px] text-accent" />
+      </SheetTrigger>
+
+      <SheetContent className="flex flex-col">
+        {/* logo */}
+        <div className="mt-32 mb-40 text-center text-2xl">
+          <Link href="/">
+            <h1 className="text-4xl font-semibold">
+              Kherin<span className="text-accent">.</span>
+            </h1>
+          </Link>
+        </div>
+
+        {/* nav */}
+        <nav className="flex flex-col justify-center items-center gap-8">
+          {links.map((link, index) => {
+            return (
+              <Link
+                href={link.path}
+                key={index}
+                className={`${
+                  link.path === pathname &&
+                  'text-accent border-b-2 border-accent'
+                } text-all capitalize hover:text-accent transition-all`}
+              >
+                {link.name}
+              </Link>
+            );
+          })}
+        </nav>
+      </SheetContent>
+    </Sheet>
+  );
+};
+
+export default MobileNav;
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,17 +1,36 @@
 /** @type {import('tailwindcss').Config} */
-export default {
+const config = {
+  darkMode: ['class'],
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
+      container: {
+        center: 'true',
+        padding: '15px',
+      },
+      screens: {
+        sm: '640px',
+        md: '768px',
+        lg: '960px',
+        xl: '1200px',
+      },
+      fontFamily: {
+        primary: 'var(--font-jetbrainsMono)',
+      },
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        primary: '#1C1C22',
+        accent: {
+          DEFAULT: '#00FF99',
+          hover: '#00E187',
+        },
       },
     },
   },
-  plugins: [],
+  plugins: [require('tailwindcss-animate')],
 };
+
+export default config;
